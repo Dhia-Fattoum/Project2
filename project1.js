0d83404b68632eb267bbdec426f8ecb12bc84c97
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-07-13 18:30:44 +0100

    not yet

--- project1.js
+++ project1.js
@@ -1,13 +1,12 @@
-var user = document.getElementById("username").value;
-var pass = document.getElementById("password").value;
-function pasuser() {
-if (user=="JavaScript") { 
-if (pass=="Kit") {              
-location="page2.html" 
-} else {
-alert("Invalid Password")
+
+function playAudio(url) {
+		 var a = new Audio(url);
+    a.play();
 }
-} else {  
-alert("Invalid UserID")
+function StopAudio(url){
+		 var a = new Audio(url);
+    a.pause();
 }
+function pause(){
+window.location.replace("C:/Users/PC/Desktop/MyProject-solo/Project2/project11.html")
 }
