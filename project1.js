b573a50097d7b97267dfe62427a099b047a557e6
Dhia-Fattoum <dhiafattoum@outlook.com>
2020-07-13 21:26:09 +0100

    last push

--- project1.js
+++ project1.js
@@ -3,10 +3,15 @@ function playAudio(url) {
 		 var a = new Audio(url);
     a.play();
 }
-function StopAudio(url){
-		 var a = new Audio(url);
-    a.pause();
-}
 function pause(){
 window.location.replace("C:/Users/PC/Desktop/MyProject-solo/Project2/project11.html")
 }
+ var songs = ["#pic1","#pic2","#pic3","#pic4","#pic5","#pic6","#pic7","#pic8","#pic9","#pic10","#pic11","#pic12","#pic13","#pic14","#pic15","#pic16"] ;
+ function random
+
+$(document).ready(function(){
+	$("#random").click(function(){
+	songs.play(songs[Math.floor(Math.random() * songs.length)])
+document.write (audiopath)
+	})
+})
